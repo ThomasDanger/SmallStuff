c8bdbcaa49e05cad12b8bd7cb60e3c55ca7a9289
Thomas <thomasconnerse@gmail.com>
2022-11-26 18:05:00 -0600

    Create Node.java

---
+++ DataStructures/Java/Stack/Node.java
@@ -0,0 +1,32 @@
+package com.mycompany.main;
+
+public class Node<G> {
+    private G data;
+    private Node<G> next;
+    
+    public Node(){
+        data=null;
+        next=null;
+    }
+    public Node(G data_){
+        data=data_;
+        next=null;
+    }
+    
+    public void setData(G data_){
+        data = data_;
+    }
+  
+    public void setNext(Node<G> next_){
+        next = next_;
+    }
+    
+    public G getData(){
+        return data;
+    }
+    
+    public Node<G> getNext(){
+        return next;
+    }
+    
+}
