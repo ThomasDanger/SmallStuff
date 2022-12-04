8ccbd6f9802de104a49fc0760821825a3f48b19b
Thomas <thomasconnerse@gmail.com>
2022-12-03 18:31:13 -0600

    Update and rename IsEven.java to IsEven-and-IsOdd.java

---
+++ BitWiseStudy/IsEven-and-IsOdd.java
@@ -0,0 +1,3 @@
+public static boolean isEven(int num){return(num&1)==0;}
+
+public static boolean isOdd(int num){return (num&1)==1;}
--- BitWiseStudy/IsEven.java
+++
@@ -1,1 +0,0 @@
-public static boolean isEven(int num){return(num&1)==0;}
